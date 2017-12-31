6fdc5a33be0b4f54de8f5a9d52f33ae9d54427db
angelitodiaz <ajjdc2@gmail.com>
2017-12-30 20:42:06 -0400

    Rutas Creadas

---
+++ src/app/components/index.paginas.ts
@@ -0,0 +1,4 @@
+//Incluimos todas las paginas que son rutas
+export {AboutComponent} from './about/about.component';
+export {ItemComponent} from './item/item.component';
+export {PortafolioComponent} from './portafolio/portafolio.component';
